4ac45725f907d444b26faedb35c67bb7cb05f1bb
lelysyj <lelysjtarigan06@gmail.com>
2026-03-13 13:21:56 +0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Layout part 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Demo Layout part 1'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+        child: Column(
+          children: [
+
+            /// HEADER
+            Container(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+
+                      Text(
+                        "Welcome, LELY",
+                        style: TextStyle(
+                          color: Color(0xFF7367F0),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 0.25,
+                        ),
+                      ),
+
+                      Text(
+                        "1202230034 - LELY SYLVA JEMIMA TARIGAN",
+                        style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF4B4B4B),
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  const CircleAvatar(
+                    radius: 20,
+                    //backgroundImage: AssetImage('assets/profpic.png'),
+                  ),
+                ],
+              ),
+            ),
+
+            /// CARD TOEFL
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 35),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Color(0xFF4839EB),
+                    Color(0xFF7367F0),
+                  ],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: const [
+
+                  SizedBox(height: 20),
+
+                  Text(
+                    'Status tes TOEFL Anda:',
+                    style: TextStyle(color: Colors.white, fontSize: 15),
+                  ),
+
+                  SizedBox(height: 8),
+
+                  Text(
+                    "LULUS",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 28,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 0.25,
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+
+                        Text(
+                          'Listening\n80',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+
+                        Text(
+                          'Structure\n80',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+
+                        Text(
+                          'Reading\n90',
+                          style: TextStyle(color: Colors.white, fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  SizedBox(height: 20),
+                ],
+              ),
+            ),
+
+            /// TITLE
+            const Text(
+              'Riwayat Tes',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.w700,
+                letterSpacing: 0.25,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
